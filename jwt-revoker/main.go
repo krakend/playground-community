6f1a7f73f4f03332b5727fd55f2c8b7347a1d617
kpacha <kpacha666@gmail.com>
2019-06-20 15:29:49 +0200

    revoker improved

--- jwt-revoker/main.go
+++ jwt-revoker/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
@@ -12,6 +13,7 @@ import (
 func main() {
 	server := flag.String("server", "krakend_ce:1234", "ip:port of the remote bloomfilter to connect to")
 	key := flag.String("key", "jti", "the name of the claim to inspect for revocations")
+	port := flag.Int("port", 8080, "port to expose the service")
 	flag.Parse()
 
 	c, err := client.New(*server)
@@ -21,50 +23,69 @@ func main() {
 	}
 	defer c.Close()
 
-	http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.New("home").Parse(indexPageContent)
+
+	http.HandleFunc("/add/", func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 		subject := *key + "-" + r.FormValue(*key)
 		c.Add([]byte(subject))
+		log.Printf("adding [%s] %s", *key, subject)
 		http.Redirect(w, r, "/", 302)
 	})
 
-	http.HandleFunc("/check", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/check/", func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 		subject := *key + "-" + r.FormValue(*key)
-		fmt.Fprintf(w, "%v", c.Check([]byte(subject)))
+		res := c.Check([]byte(subject))
+		log.Printf("checking [%s] %s => %v", *key, subject, res)
+		fmt.Fprintf(w, "%v", res)
 	})
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, _ *http.Request) {
 		rw.Header().Add("Content-Type", "text/html")
-		rw.Write(indexPageContent)
+		tmpl.Execute(rw, *key)
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
 
-var indexPageContent = []byte(`
-<!DOCTYPE html>
-<html>
+const indexPageContent = `
+<!doctype html>
+<html lang="en">
   <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>KrakenD JWT Revoker</title>
     <meta name="description" content="KrakenD JWT Revoker - Revoke your JWT remotely.">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-    <h1>KrakenD JWT Revoker - Revoke your JWT remotely.</h1>
-    <h2>Add</h2>
-	<form action="/add">
-	  jti:<br>
-	  <input type="text" name="jti" value="the jti to revoke">
-	  <br><br>
-	  <input type="submit" value="Submit">
-	</form>
-    <h2>Check</h2>
-	<form action="/check">
-	  jti:<br>
-	  <input type="text" name="jti" value="the jti to revoke">
-	  <br><br>
-	  <input type="submit" value="Submit">
-	</form>
+  <div class="container">
+    <h1>KrakenD JWT Revoker</h1>
+    <h2>Revoke your JWT remotely.</h2>
+    <div class="row">
+    <div class="col-sm">
+	    <h3>Add</h3>
+		<form action="/add/">
+		  {{.}}:<br>
+		  <input type="text" name="{{.}}" value="the {{.}} to revoke">
+		  <br><br>
+		  <input type="submit" value="Submit">
+		</form>
+    </div>
+    <div class="col-sm">
+	    <h3>Check</h3>
+		<form action="/check/">
+		  {{.}}:<br>
+		  <input type="text" name="{{.}}" value="the {{.}} to revoke">
+		  <br><br>
+		  <input type="submit" value="Submit">
+		</form>
+    </div>
+  </div>
+    
+
+  </div>
   </body>
 </html>
-`)
+`
